dcc1b4988ddc97239de1bb937e1b036b0231598b
guiarthur <arthur.guillaume@ynov.com>
2025-10-07 15:16:17 +0200

    création du serveur

--- server.go
+++ server.go
@@ -1,1 +1,22 @@
-package power4
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("./index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	tmpl.Execute(w, nil)
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
