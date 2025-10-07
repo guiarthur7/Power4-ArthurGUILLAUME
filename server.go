6f677879d3362098688fcdc2143d979bd70bfb48
guiarthur <arthur.guillaume@ynov.com>
2025-10-07 20:11:07 +0200

    ajout des modes facile moyen et hard

--- server.go
+++ server.go
@@ -19,7 +19,7 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, nil)
 }
 
-func Jeu(w http.ResponseWriter, r *http.Request) {
+func JeuFacile(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		joueur := Player{
 			Name1: r.FormValue("name1"),
@@ -35,12 +35,49 @@ func Jeu(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func JeuNormal(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		joueur := Player{
+			Name1: r.FormValue("name1"),
+			Name2: r.FormValue("name2"),
+		}
+		tmpl, err := template.ParseFiles("./game/powernormal.html", "./template/info_joueurs.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		tmpl.Execute(w, joueur)
+	} else {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+}
+
+func JeuHard(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		joueur := Player{
+			Name1: r.FormValue("name1"),
+			Name2: r.FormValue("name2"),
+		}
+		tmpl, err := template.ParseFiles("./game/powerhard.html", "./template/info_joueurs.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		tmpl.Execute(w, joueur)
+	} else {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", Home)
-	http.HandleFunc("/powerfacile.html", Jeu)
+	http.HandleFunc("/powerfacile.html", JeuFacile)
+	http.HandleFunc("/powernormal.html", JeuNormal)
+	http.HandleFunc("/powerhard.html", JeuHard)
+
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	imFS := http.FileServer(http.Dir("src/"))
 	http.Handle("/src/", http.StripPrefix("/src/", imFS))
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
