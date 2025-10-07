6da23399cd9fd3463242f1d44f5db6a0fb11d2b2
guiarthur <arthur.guillaume@ynov.com>
2025-10-07 18:57:21 +0200

    fixhome

--- server.go
+++ server.go
@@ -6,6 +6,11 @@ import (
 	"net/http"
 )
 
+type Player struct {
+	Name1 string
+	Name2 string
+}
+
 func Home(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("./index.html")
 	if err != nil {
@@ -14,9 +19,28 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, nil)
 }
 
+func Jeu(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		joueur := Player{
+			Name1: r.FormValue("name1"),
+			Name2: r.FormValue("name2"),
+		}
+		tmpl, err := template.ParseFiles("./game/power.html", "./template/info_joueurs.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		tmpl.Execute(w, joueur)
+	} else {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", Home)
+	http.HandleFunc("/power.html", Jeu)
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	imFS := http.FileServer(http.Dir("src/"))
+	http.Handle("/src/", http.StripPrefix("/src/", imFS))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
