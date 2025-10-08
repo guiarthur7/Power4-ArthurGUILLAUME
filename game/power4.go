1ad24409a26180e9778d3013f3b483a4e3a47a71
guiarthur <arthur.guillaume@ynov.com>
2025-10-08 16:37:14 +0200

    msg

--- game/power4.go
+++ game/power4.go
@@ -1,19 +1,1 @@
 package power4
-
-rows := 6
-
-col := 7
-
-type Jeu stuct {
-	Grille [][]uint8
-	Player uint8
-}
-
-func Switch(game *Jeu) {
-	if game.Player == 1 {
-		game.Player = 2
-	} 
-	if game.Player == 2 {
-		game.Player = 1
-	}
-}
